9d6bfee8538fb73158ff99384977e4cbc1f85b01
Malick DIOP <diop-malick@hotmail.fr>
2017-06-09 13:21:48 +0000

    Add domain & repository

---
+++ src/main/java/heroes/backend/config/RepositoryConfig.java
@@ -0,0 +1,15 @@
+package heroes.backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
+
+import heroes.backend.domain.Hero;
+
+@Configuration
+public class RepositoryConfig extends RepositoryRestConfigurerAdapter {
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+        config.exposeIdsFor(Hero.class);
+    }
+}
